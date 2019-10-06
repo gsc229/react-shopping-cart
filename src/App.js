b8d866af4ec79b9860d1018bb81088a756605582
Greg <gsc229@gmail.com>
2019-10-06 18:45:50 -0400

    Products component consuming products from ProductContext

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from './contexts/ProductContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -8,35 +9,28 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		setCart([...cart, item]);
-	};
+  const addItem = item => {
+    setCart([...cart, item]);
+  };
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+  return (
+    <div className='App'>
+      <ProductContext.Provider value={{ products, addItem }}>
+        <Navigation cart={cart} />
+      </ProductContext.Provider>
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
-	);
+      {/* Routes */}
+      <ProductContext.Provider value={{ products, addItem }}>
+        <Route exact path='/' component={Products} />
+      </ProductContext.Provider>
+      <ProductContext.Provider value={{ products, addItem }}>
+        <Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
+      </ProductContext.Provider>
+    </div>
+  );
 }
 
 export default App;
