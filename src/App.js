530f94be88a16aa1895071635afaf9805716a0f9
Greg <gsc229@gmail.com>
2019-10-06 19:48:38 -0400

    ShoppingCart and Navigation consuming cart form CartContext

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+// contexts
 import { ProductContext } from './contexts/ProductContext';
-
+import { CartContext } from './contexts/CartContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -19,7 +20,9 @@ function App() {
   return (
     <div className='App'>
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
+        <CartContext.Provider value={cart}>
+          <Navigation />
+        </CartContext.Provider>
       </ProductContext.Provider>
 
       {/* Routes */}
@@ -27,7 +30,9 @@ function App() {
         <Route exact path='/' component={Products} />
       </ProductContext.Provider>
       <ProductContext.Provider value={{ products, addItem }}>
-        <Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
+        <CartContext.Provider value={cart}>
+          <Route path='/cart' component={ShoppingCart} />
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
